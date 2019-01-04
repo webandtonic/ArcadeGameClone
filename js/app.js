3f9ea1c4372be01ee4421ab9eb51ef7039a795ad
Kerstin Hofmann <kerstinhofmann@Kerstins-iMac.fritz.box>
2019-01-04 13:21:24 +0100

    added collision detection

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function(row, speed) {
-    this.start = Math.random();
-    this.x = -101 - (1500 * this.start);
+let Enemy = function(row, speed) {
+    this.x = -101 - (2000 * Math.random());
     this.y = (row * 83) - 20;
+    this.row = row;
     this.speed = speed;
-
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -12,11 +11,11 @@ var Enemy = function(row, speed) {
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     if (this.x > 505) {
-      this.x = -101 - (1500 * this.start);
+      this.x = -101 - (2000 * Math.random());
     } else {
       this.x += (150 * dt * this.speed);
     }
-
+    this.head = this.x + 101;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,25 +23,35 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-let allEnemies = [];
-
-for (i = 1; i < 4; i++) {
-  for (j = 1; j < 4; j++) {
-    let bug = new Enemy(i, j);
-    allEnemies.push(bug);
-  }
-}
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
+let Player = function() {
     this.x = 202;
     this.y = 404;
     this.sprite = 'images/char-boy.png';
 
     this.update = function() {
-        // check collision/position here
+
+//define left and right border of actual image (without transparent background)
+        this.rightHand = this.x + 80;
+        this.leftHand = this.x + 21;
+
+// check position of player and call function if indicated
+        switch (this.y) {
+          case -11:
+            this.gameWon = true;
+            break;
+          case 72:
+            row1Enemies.forEach(this.checkCollisions);
+            break;
+          case 155:
+            row2Enemies.forEach(this.checkCollisions);
+            break;
+          case 238:
+            row3Enemies.forEach(this.checkCollisions);
+
+        }
     };
 
     this.render = function() {
@@ -70,12 +79,15 @@ var Player = function() {
           if (this.y < 404) {
             this.y += 83;
           };
-          break;
       }
     }
+
+    this.checkCollisions = function (bug) {
+      if (bug.head > player.leftHand && bug.x < player.rightHand) {
+              player.y = 404;
+            }
+          };
 };
-
-
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -83,10 +95,28 @@ var Player = function() {
 
 let player = new Player();
 
+let allEnemies = [];
+
+// create 3 enemy bugs for each row with a different speed each, and add to array
+
+for (i = 1; i < 4; i++) {
+  for (j = 1; j < 4; j++) {
+    let bug = new Enemy(i, j);
+    allEnemies.push(bug);
+  }
+}
+
+// create 3 sub-arrays with bugs of each row
+
+let row1Enemies = allEnemies.filter(bug => bug.row == 1);
+let row2Enemies = allEnemies.filter(bug => bug.row == 2);
+let row3Enemies = allEnemies.filter(bug => bug.row == 3);
+
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
